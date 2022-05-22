3e1d2ff055db8d554ae33905ac51366ac652e168
Remus Cristian Mocanu <remuscristian.mocanu@bnc.ca>
2022-05-22 11:01:56 -0400

    first commit for deployment, not complete

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,12 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=kwoj@r2flxhzbfl*(t(mf_&07grodtg%0z-1rkuhj&+y1pc9@'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
